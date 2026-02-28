15ef3ee991a99e2d2ec736ee7b09e62cd8dacc3c
Ramfour <bdddanil2002@gmail.com>
2026-02-28 22:28:57 +0700

    add css for index | finish task5

--- task5/script.js
+++ task5/script.js
@@ -1,11 +1,18 @@
-const button =  document.querySelector('button');
-button.addEventListener('click', ()=>{
-    
-});
+function updateInput (element){
+   duplicateField.textContent =  this.value;
+}
+
 
 const duplicateField = document.querySelector('#duplicateField');
 const input =  document.querySelector('input');
-input.addEventListener('input', function(e){
-   duplicateField.textContent =  this.value;
+input.addEventListener('input', updateInput);
+
+const button =  document.querySelector('button');
+button.addEventListener('click', ()=>{
+       console.log('Текст поля ввода: '+input.value);
+       input.value = '';
+       updateInput(input.value);
 });
+
+
 
